f2dd65fdbd564f0735080b2930dc78a8fc68b9f8
Hermii1 <hermelaejigu4@gmail.com>
2025-05-16 13:35:00 +0300

    updated layout

--- src/Components/Layouts.jsx
+++ src/Components/Layouts.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import React from 'react';
 import Header from './Header.jsx';
-import Footer from './footer.jsx';
+import Footer from './Footer.jsx';
 
 const Layout = () => {
   return (
